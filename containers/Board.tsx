40c582533f36444ee5aaaac1fa01e15b9aca5e97
sara pulido <sara01rizo@gmail.com>
2023-03-29 12:16:39 +0200

    Create: basis function for board container

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,8 +1,41 @@
 import { useState } from "react";
+import Square from "../components/Square";
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  return <div>Board</div>;
+  const [currentPlayer, setCurrentPlayer] = useState<'X' | '0'>(
+    Math.round(Math.random() * 1) === 1 ? 'X' : '0'
+  );
+  const [winner, setWinner] = useState(null);
+
+  function setSquareValue(index){
+    const newData = squares.map((val, i) =>{
+      if(i === index){
+        return currentPlayer;
+      }
+      return val;
+    });
+    setSquares(newData)
+    setCurrentPlayer(currentPlayer === 'X' ? '0' : 'X')
+  }
+  return (
+    <div>
+      <p>Hey {currentPlayer}, it's your turn</p>
+
+      {Array(9)
+        .fill(null)
+        .map((_, i) => {
+          return (
+            <Square
+              winner={winner}
+              key={i}
+              onClick={() => setSquareValue(i)}
+              value={squares[i]}
+            />
+          );
+      })}
+    </div>
+  );
 }
 
 
